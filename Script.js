5f33ef6d038a7ed4a7d8f69255ac945491a103d7
Turbotor <63958625+Turbotor@users.noreply.github.com>
2020-08-23 17:40:04 +0200

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,27 @@
+// Question 1
+const button = document.querySelector("Bg");
+button.onclick = function () {
+console.log(event.target);
+event.target.style.background = "green";
+}
+
+//question 2
+const button = document.querySelector("Toggler");
+button.onclick = function () {
+console.log(event.target);
+event.target.classList.extra
+ <div class="togglee">
+ }
+ 
+ //question 3 
+ const dropdown = document.querySelector("select");
+ const content = document.querySelector("paragraphs-container");
+ dropdown.onchange = function() {
+ console.log(event.target.value);
+ const selectedValue = event.target.value
+ for(let i = 1; i <= seletctedValue; i++) {
+ paragraphs-container.innerHTML += `<div class="content"<>/div´;
+ }
+ };
+ //Q4&Q5
+ 
